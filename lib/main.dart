71b79ecdc050ba4f192826326a3b996798c9c119
AndresLince <lynxdarmatron@gmail.com>
2023-06-27 05:40:46 -0500

    Add buttons with different sounds and colors WIP

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,79 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () async {
-                final player = AudioPlayer();
-                await player.play(AssetSource('note1.wav'));
-              },
-              child: Text('Click me'),
-            ),
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(AssetSource('note1.wav'));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+                child: Text('Click me'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(AssetSource('note2.wav'));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                ),
+                child: Text('Click me'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(AssetSource('note3.wav'));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                ),
+                child: Text('Click me'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(AssetSource('note4.wav'));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                ),
+                child: Text('Click me'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(AssetSource('note5.wav'));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.cyan,
+                ),
+                child: Text('Click me'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(AssetSource('note6.wav'));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                child: Text('Click me'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.play(AssetSource('note7.wav'));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                ),
+                child: Text('Click me'),
+              ),
+            ],
           ),
         ),
       ),
